55f129339d0a563559caf782b32f314757936851
shivenhanda <shivenhanda2003@gmail.com>
2025-12-28 18:53:35 +0530

    Add cors

--- backend/script.js
+++ backend/script.js
@@ -3,15 +3,22 @@ import path from 'path';
 import { calculation } from './calculation.js';
 import {calculationdegree} from './calculationdegree.js'
 import dotenv from 'dotenv'; 
+import cors from 'cors';
 dotenv.config();
 const app = express();
 
 const staticPath = path.join(process.cwd(), "../frontend/dist")
 app.use(express.static(staticPath))
 app.use(express.json())
+app.use(cors({
+    origin: "https://calculator1-0ha8.onrender.com",
+    methods: ["GET", "POST"], 
+    credentials: true
+}))
 app.get("/", (req, res) => {
     res.sendFile(path.join(staticPath, "index.html"))
 }
+
 )
 app.post("/calculate", (req, res) => {
     const { expression } = req.body;
